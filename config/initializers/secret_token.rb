04b48293a73a79e7319a7012c74d50882fbc7fed
Justin Coyne <justin@curationexperts.com>
2013-04-08 13:58:05 -0500

    Generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tufts::Application.config.secret_token = '6dc85335dfd41f3ed485958c688e155803ab71baf25c428faafbcf94855ec2579e9c9668a4a5c42128d25ab13e880fbc7c65c73e46f42b8f078369e928373230'
