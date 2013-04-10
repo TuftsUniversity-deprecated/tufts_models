02e809b807af92ffe937d43e5c937fa58dc5e611
Justin Coyne <justin@curationexperts.com>
2013-04-10 17:24:06 -0500

    Ensure the load order happens same on every system

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,4 +1,6 @@
 # spec/support/features.rb
+require "#{Rails.root}/spec/support/features/session_helpers"
+
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
 end
