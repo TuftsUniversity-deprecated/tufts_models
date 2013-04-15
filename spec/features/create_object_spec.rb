fce024be76455b0b12e1d87cf8f83fb70a564a71
Justin Coyne <justin@curationexperts.com>
2013-04-15 14:02:00 -0500

    Test for creating an object

--- spec/features/create_object_spec.rb
+++ spec/features/create_object_spec.rb
@@ -9,6 +9,13 @@ feature 'Admin user creates document' do
     click_link 'Create a new object'
 
     select "Audio", from: 'Type' 
+    click_button 'Next'
+
+    fill_in '*Title', with: 'My title'
+    click_button 'Save'
+
+    page.should have_selector('h1', text: 'My title')
+
   end
 
 end
