9b58c929101f93f4814e9fd324eb4fd39377b0ad
Justin Coyne <justin@curationexperts.com>
2013-04-16 17:41:49 -0500

    Add routes that allow periods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Tufts::Application.routes.draw do
   root :to => "catalog#index"
 
-  Blacklight.add_routes(self)
+  Blacklight::Routes.new(self, {}).catalog
+  # This is from Blacklight::Routes#solr_document, but with the constraints added which allows periods in the id
+  resources :solr_document,  :path => 'catalog', :controller => 'catalog', :only => [:show, :update] 
+  resources :catalog, :only => [:show, :update], :constraints => { :id => /[a-zA-Z0-9.:]+/ }
+  
   HydraHead.add_routes(self)
   mount FcrepoAdmin::Engine => '/admin', :as=> 'fcrepo_admin'
   mount HydraEditor::Engine => '/'
