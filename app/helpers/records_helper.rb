2f921e778a0dabcf12c958223a6ae9676898743a
Justin Coyne <justin@curationexperts.com>
2013-04-16 12:06:19 -0500

    Display the type we are editing

--- app/helpers/records_helper.rb
+++
@@ -1,8 +0,0 @@
-module RecordsHelper
-  include RecordsHelperBehavior
-
-  
-  def object_type_options
-    {'Audio' => 'TuftsAudio', 'PDF' => 'TuftsPdf'}
-  end
-end
--- spec/helpers/records_helper_spec.rb
+++ spec/helpers/records_helper_spec.rb
@@ -4,4 +4,9 @@ describe RecordsHelper do
   it "should have object_type_options" do
     helper.object_type_options.should == {'Audio' => 'TuftsAudio', 'PDF' => 'TuftsPdf'}
   end
+
+  it "should have model_labels" do
+    helper.model_label('TuftsAudio').should == 'audio'
+    helper.model_label('TuftsPdf').should == 'PDF'
+  end
 end
