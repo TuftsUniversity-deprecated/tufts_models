f927a3366ddce75856cacf3408929b55e9f1018c
Justin Coyne <justin@curationexperts.com>
2013-04-19 16:43:56 -0500

    Add datasterams to TuftsImageText

--- app/models/tufts_image_text.rb
+++ app/models/tufts_image_text.rb
@@ -1,4 +1,10 @@
 class TuftsImageText < TuftsBase
+  has_file_datastream 'Content.html', control_group: 'E'
+
+  has_file_datastream 'Thumbnail.png', control_group: 'E'
+  has_file_datastream 'Archival.tif', control_group: 'E'
+  has_file_datastream 'Advanced.jpg', control_group: 'E'
+  has_file_datastream 'Basic.jpg', control_group: 'E'
   def self.to_class_uri
     'info:fedora/cm:Object.Generic'
   end
