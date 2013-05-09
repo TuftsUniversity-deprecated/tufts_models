34ed6ed2ffef685ad3103b477ab489da815d3ad0
Justin Coyne <justin@curationexperts.com>
2013-05-09 13:53:59 -0500

    Update form for ead and generic objects

--- app/models/tufts_generic_object.rb
+++ app/models/tufts_generic_object.rb
@@ -1,8 +1,27 @@
 class TuftsGenericObject < TuftsBase
   has_metadata :name => "GENERIC-CONTENT", :type => TuftsGenericMeta
+  #has_file_datastream 'Archival.pdf', control_group: 'E', original: true
 
   def self.to_class_uri
     'info:fedora/cm:Object.Generic'
   end
 
+
+  # Given a datastream name, return the local path where the file can be found.
+  # If an extension is provided, generate the path, if the extension is not provided,
+  # derive it from the stored dsLocation
+  # @example
+  #   obj.file_path('GENERIC-CONTENT', 'zip')
+  #   # => /local_object_store/data01/tufts/central/dca/MS054/archival_tif/MS054.003.DO.02108.archival.tif
+  def file_path(name, extension=nil)
+    File.join(directory_for(name), "#{pid_without_namespace}.#{extension}")
+  end
+
+  # Given a datastream name, return the directory path where the file can be found.
+  # @example
+  #   obj.file_path('GENERIC-CONTENT')
+  #   # => /local_object_store/data01/tufts/central/dca/MS054/generic
+  def directory_for(name)
+    File.join(collection_id, 'generic')
+  end
 end
--- spec/models/tufts_generic_object_spec.rb
+++ spec/models/tufts_generic_object_spec.rb
@@ -25,4 +25,33 @@ describe TuftsGenericObject do
     its(:to_class_uri) {should == 'info:fedora/cm:Object.Generic'}
   end
 
+  # it "should have an original_file_datastream" do
+  #   TuftsGenericObject.original_file_datastream.should == "GENERIC-CONTENT"
+  # end
+
+#   <foxml:datastream ID="GENERIC-CONTENT" STATE="A" CONTROL_GROUP="X" VERSIONABLE="true">
+# <foxml:datastreamVersion ID="GENERIC-CONTENT.0" LABEL="Enter a label here." CREATED="2009-10-16T15:56:59.270Z" MIMETYPE="text/xml" SIZE="411">
+# <foxml:xmlContent>
+# <content xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.fedora.info/definitions/">
+#   					        <item id="0">
+#     					         <link>http://bucket01.lib.tufts.edu/data05/tufts/central/dca/MS115/generic/MS115.003.001.00003.zip</link>
+#     					         <fileName>MS115.003.001.00003</fileName>
+#     					         <mimeType>application/zip</mimeType>
+#   					        </item>
+# 				        </content>
+# </foxml:xmlContent>
+# </foxml:datastreamVersion>
+# </foxml:datastream>
+
+  describe "an generic content with a pid" do
+    before do
+      subject.inner_object.pid = 'tufts:MS054.003.DO.02108'
+    end
+    it "should give a remote url" do
+      subject.remote_url_for('GENERIC-CONTENT', 'zip').should == 'http://bucket01.lib.tufts.edu/data01/tufts/central/dca/MS054/generic/MS054.003.DO.02108.zip'
+    end
+    it "should give a local_path" do
+      subject.local_path_for('GENERIC-CONTENT', 'zip').should == "#{Rails.root}/spec/fixtures/local_object_store/data01/tufts/central/dca/MS054/generic/MS054.003.DO.02108.zip"
+    end
+  end
 end
