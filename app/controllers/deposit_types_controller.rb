b9f830c3294934c226ea5116ebf6a6361737bef2
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-10-07 23:25:26 +0800

    Initial views for deposit types, issue #40

--- app/controllers/deposit_types_controller.rb
+++ app/controllers/deposit_types_controller.rb
@@ -1,2 +1,22 @@
 class DepositTypesController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @deposit_type = TuftsDepositType.find(params[:id])
+  end
+
+  def create
+    @deposit_type = TuftsDepositType.new(deposit_type_params)
+
+    @deposit_type.save!
+    puts @deposit_type.inspect
+    redirect_to deposit_type_path(@deposit_type)
+  end
+
+  private
+  def deposit_type_params
+    params.require(:deposit_type).permit(:display_name, :deposit_agreement)
+  end
 end
