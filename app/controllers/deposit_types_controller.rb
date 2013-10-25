459690716635f5e7cf4a17730525b4bd6de403eb
val99erie <val99erie@gmail.com>
2013-10-25 16:51:05 -0500

    Added authorization to deposit types controller.  Issue #55

--- app/controllers/deposit_types_controller.rb
+++ app/controllers/deposit_types_controller.rb
@@ -1,12 +1,10 @@
 class DepositTypesController < ApplicationController
+  load_and_authorize_resource
 
   def index
-    authorize! :read, DepositType
-    @deposit_types = DepositType.accessible_by(current_ability)
   end
 
   def export
-    authorize! :export, DepositType
     require 'import_export/deposit_type_exporter'
     exporter = DepositTypeExporter.new
     exporter.export_to_csv
@@ -19,39 +17,34 @@ class DepositTypesController < ApplicationController
   end
 
   def show
-    @deposit_type = DepositType.find(params[:id])
   end
 
   def destroy
-    @deposit_type = DepositType.find(params[:id])
     @deposit_type.destroy
-
     redirect_to deposit_types_path
   end
 
   def create
-    @deposit_type = DepositType.new(deposit_type_params)
-
-    @deposit_type.save!
-    puts @deposit_type.inspect
-    redirect_to deposit_type_path(@deposit_type)
+    if @deposit_type.save
+      redirect_to deposit_type_path(@deposit_type)
+    else
+      render :new
+    end
   end
 
   def edit
-    @deposit_type = DepositType.find(params[:id])
   end
 
   def update
-    @deposit_type = DepositType.find(params[:id])
-
-    if @deposit_type.update_attributes!(params[:deposit_type].permit(:display_name, :deposit_agreement, :deposit_view))
+    if @deposit_type.update_attributes(deposit_type_params)
       redirect_to deposit_type_path(@deposit_type), :notice => 'Record was successfully updated.'
     else
       render 'edit'
     end
   end
 
-  private
+private
+
   def deposit_type_params
     params.require(:deposit_type).permit(:display_name, :deposit_agreement, :deposit_view)
   end
