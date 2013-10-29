de588539bb282214dd0d153294d240095b6ac48b
val99erie <val99erie@gmail.com>
2013-10-29 11:43:35 -0500

    Facet on accrual policy to allow user to easily find self-deposited docs.  Issue #61

--- spec/features/unpublished_list_spec.rb
+++ spec/features/unpublished_list_spec.rb
@@ -11,6 +11,7 @@ feature 'View unpublished documents' do
 
     sign_in :admin
   end
+
   scenario 'with a TuftsAudio' do
     visit root_path
     click_link 'Unpublished objects'
@@ -22,6 +23,25 @@ feature 'View unpublished documents' do
     page.should_not have_link('Very unique title', href: catalog_path(@production) )
   end
 
-end
+  describe 'faceting to find self-deposited documents' do
+    before do
+      @self_deposit = TuftsAudio.new(title: 'self-d', description: 'abc', creator: 'Fred', accrualPolicy: 'Submitted via contributor self-deposit')
+      @self_deposit.save!
+    end
+    after do
+      @self_deposit.destroy
+    end
 
+    it 'shows only the self-deposited docs' do
+      visit unpublished_index_path
+      within(".blacklight-accrual_sim") do
+        facet_link_for_self_deposit = "Submitted via Contributor Self-Deposit"
+        click_link facet_link_for_self_deposit
+      end
+      page.should have_css('#documents .document', count: 1) # filtered out all but one
+      page.should have_link(@self_deposit.title, href: catalog_path(@self_deposit))
+    end
+  end
+
+end
 
