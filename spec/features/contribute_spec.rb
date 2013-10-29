88c843d35ee77600a5a3bbd8a2674d0a279196b0
Mark Bussey <mark@curationexperts.com>
2013-10-29 01:42:18 -0500

    Add dynamically generated select list of deposit types on /contribute landing page for authenticated users

--- spec/features/contribute_spec.rb
+++ spec/features/contribute_spec.rb
@@ -15,6 +15,15 @@ describe 'Contribute' do
   end
 
   describe "Landing Page" do
+    before :all do
+      @deposit_type = DepositType.new(:display_name => 'Test Option', :deposit_view => 'generic_deposit', :deposit_agreement => 'Legal links here...')
+      @deposit_type.save!
+    end
+
+    after :all do
+      @deposit_type.destroy
+    end
+
     describe 'for unauthenticated users' do
       before :all do
         visit destroy_user_session_path
@@ -28,6 +37,10 @@ describe 'Contribute' do
         expect(page).to have_content 'Tufts Simplified Sign-On Enabled'
         expect(page).to have_link 'Login'
       end
+      it 'should show configured deposit type options' do
+        visit '/contribute'
+        expect(page).to have_content 'Test Option'
+      end
     end
     describe 'for authenticated users' do
       before :each do
@@ -36,6 +49,13 @@ describe 'Contribute' do
       it 'should exist' do
         visit '/contribute'
         current_path.should == contribute_path
+      end
+      it 'should let users select a deposit type' do
+        visit '/contribute'
+        page.should have_select 'deposit_type'
+      end
+      it 'should provide a button to create new deposits' do
+        page.should have_button 'Begin'
       end
     end
   end
