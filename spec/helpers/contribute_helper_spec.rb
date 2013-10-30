96cf1c544df3a1db206cdacd8d3029710e86b131
val99erie <val99erie@gmail.com>
2013-10-30 09:31:56 -0500

    Removed some unneeded files and some pending statements that were spamming up the test suite

--- spec/helpers/contribute_helper_spec.rb
+++ spec/helpers/contribute_helper_spec.rb
@@ -11,5 +11,4 @@ require 'spec_helper'
 #   end
 # end
 describe ContributeHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/contribute/new.html.erb_spec.rb
+++ spec/views/contribute/new.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe "contribute/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "test that the form elements are correct"
 end
