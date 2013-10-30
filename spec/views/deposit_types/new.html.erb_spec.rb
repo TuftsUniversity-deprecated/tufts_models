fd29e89ecc64c9ce6f17bf719e23294042dfa616
val99erie <val99erie@gmail.com>
2013-10-30 15:58:56 -0500

    Cleaned up views for DepositType.  Issue #64

--- spec/views/deposit_types/new.html.erb_spec.rb
+++ spec/views/deposit_types/new.html.erb_spec.rb
@@ -1,29 +1,28 @@
 require 'spec_helper'
 
 describe 'deposit_types/new.html.erb' do
+  before :each do
+    assign :deposit_type, DepositType.new
+    render
+  end
 
   it 'should have a display_name field' do
-    render
     rendered.should have_field('deposit_type[display_name]', type: 'text')
   end
 
   it 'should have a deposit_view field' do
-    render
     rendered.should have_field('deposit_type[deposit_view]', type: 'select')
   end
 
   it 'should have a deposit_agreement field' do
-    render
     rendered.should have_field('deposit_type[deposit_agreement]', type: 'textarea')
   end
 
   it 'should have a Create button' do
-    render
-    rendered.should have_button('Create')
+    rendered.should have_button('Save')
   end
 
   it 'should have a Cancel link' do
-    render
     rendered.should have_link('Cancel', href: deposit_types_path)
   end
 
