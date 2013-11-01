f285f518935259131055bc35c84d510f87c5cd70
Justin Coyne <justin@curationexperts.com>
2013-11-01 09:36:16 -0500

    Test to ensure we don't allow access to items a user doesn't own.  Fixes #67

--- spec/factories/tufts_audio.rb
+++ spec/factories/tufts_audio.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :tufts_self_deposit do
+  factory :tufts_audio do
     ignore do
       user { FactoryGirl.create(:user) }
     end
@@ -7,6 +7,6 @@ FactoryGirl.define do
     after(:build) { |deposit, evaluator|
       deposit.apply_depositor_metadata(evaluator.user.user_key)
     }
-    rights { 'http://dca.tufts.edu/ua/access/rights-creator.html' }
   end
 end
+
