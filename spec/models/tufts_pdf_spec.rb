01c076adcdba19ace394b10f58154cbe9214313c
Justin Coyne <justin@curationexperts.com>
2013-11-04 12:46:07 -0600

    Refactor ability test out of the TuftsPdfSpec

--- spec/models/tufts_pdf_spec.rb
+++ spec/models/tufts_pdf_spec.rb
@@ -1,25 +1,6 @@
 require 'spec_helper'
 
 describe TuftsPdf do
-  
-  describe "with access rights" do
-    before do
-      @pdf = TuftsPdf.new(title: 'test pdf')
-      @pdf.read_groups = ['public']
-      @pdf.save!
-    end
-
-    after do
-      @pdf.destroy
-    end
-
-    let (:ability) {  Ability.new(nil) }
-
-    it "should be visible to a not-signed-in user" do
-      ability.can?(:read, @pdf.pid).should be_true
-    end
-  end
-
   describe "to_class_uri" do
     subject {TuftsPdf}
     its(:to_class_uri) {should == 'info:fedora/cm:Text.PDF'}
