ae1943608f810701acfd4aad036227ea6c244498
Justin Coyne <justin@curationexperts.com>
2013-11-11 14:09:43 -0600

    Qualifying papers should have a description prefix. Fixes #119

---
+++ spec/models/qualifying_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe QualifyingPaper do
+  describe "description" do
+    before do
+      subject.description = 'student provided description'
+    end
+
+    it "should get prefixed" do
+      expect(subject.tufts_pdf.description).to eq ["A qualifying paper submitted in partial fulfillment of the requirements for the degree of Doctor of Philosophy in CATALOGER-FIX-ME Education. Abstract: student provided description"]
+    end
+  end
+end
