3236b865c5725e94e7a9c7994c0cb00cc825fb3a
Justin Coyne <justin@curationexperts.com>
2013-11-12 11:21:21 -0600

    Store self-deposit PDFs in the correct directory. Fixes #113

--- spec/models/tufts_pdf_spec.rb
+++ spec/models/tufts_pdf_spec.rb
@@ -29,6 +29,20 @@ describe TuftsPdf do
     before do
       subject.inner_object.pid = 'tufts:MS054.003.DO.02108'
     end
+    describe "and a collection" do
+      let(:collection_id) { 'tufts:UA069.001.DO.UA015' }
+      before do
+        unless ActiveFedora::Base.exists? collection_id
+          ActiveFedora::FixtureLoader.new('spec/fixtures').import_and_index(collection_id)
+        end
+        subject.collection_id = collection_id
+      end
+      it "should give a remote URL" do
+        #http://bucket01.lib.tufts.edu/data05/tufts/central/dca/UA015/archival_pdf/{#PID}.archival.pdf
+        subject.remote_url_for('Archival.pdf', 'pdf').should == 'http://bucket01.lib.tufts.edu/data01/tufts/central/dca/UA015/archival_pdf/MS054.003.DO.02108.archival.pdf'
+      end
+
+    end
     it "should give a remote url" do
       subject.remote_url_for('Archival.pdf', 'pdf').should == 'http://bucket01.lib.tufts.edu/data01/tufts/central/dca/MS054/archival_pdf/MS054.003.DO.02108.archival.pdf'
     end
