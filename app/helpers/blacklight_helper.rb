5f05db9ee624a62ff7c06580fcf1fa44fd3c914a
val99erie <val99erie@gmail.com>
2014-03-10 16:10:20 -0500

    Display the template's title at the head of the show page.  Plus some refactoring of the link name on the catalog page.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -2,9 +2,18 @@ require Blacklight::Engine.root.join('app/helpers/blacklight_helper')
 
 module BlacklightHelper
 
-  def doc_label(document)
-    label = Array(document['template_title_tesim']).first
-    label ||= document_show_link_field(document)
+  def document_show_link_field(document=nil)
+    field = if document[:template_title_tesim]
+              :template_title_tesim
+            else
+              super  # super from blacklight
+            end
+  end
+
+  def document_heading(document=nil)
+    document ||= @document
+    label = document[:template_title_tesim]
+    label ||= super  # super from blacklight
   end
 
 end
--- spec/helpers/blacklight_helper_spec.rb
+++ spec/helpers/blacklight_helper_spec.rb
@@ -2,19 +2,34 @@ require 'spec_helper'
 
 describe BlacklightHelper do
 
-  describe 'the label for the link to the object:' do
+  before do
+    config = CatalogController.new.blacklight_config
+    helper.stub(:blacklight_config).and_return(config)
+  end
 
+  describe '#document_show_link_field' do
     it 'for a template object, it displays the template_title' do
       doc = SolrDocument.new('template_title_tesim' => ['My Template'])
-      helper.doc_label(doc).should == 'My Template'
+      helper.document_show_link_field(doc).should == :template_title_tesim
     end
 
-    it 'for a non-template object, it uses normal blacklight behavior' do
+    it 'for a non-template object, it falls back to normal blacklight behavior' do
       doc = SolrDocument.new('title_tesim' => ['My Title'])
-      helper.should_receive(:document_show_link_field).and_return('My Title')
-      helper.doc_label(doc).should == 'My Title'
+      helper.document_show_link_field(doc).should == :title_tesim
     end
 
+  end
+
+  describe '#document_heading' do
+    it 'for a template object, it displays the temlate_title' do
+      doc = SolrDocument.new('template_title_tesim' => ['My Template'])
+      helper.document_heading(doc).should == ['My Template']
+    end
+
+    it 'for a non-template object, it falls back to normal blacklight behavior' do
+      doc = SolrDocument.new('title_tesim' => ['My Title'])
+      helper.document_heading(doc).should == ['My Title']
+    end
   end
 
 end
