c504a07f5c019729e1f2c343e9fd3f1ff55236ef
val99erie <val99erie@gmail.com>
2014-03-19 13:42:42 -0500

    Added comments to explain why these methods are here

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,6 +1,10 @@
 module BlacklightHelper
   include Hydra::BlacklightHelperBehavior
 
+  # This method controls the name of the link for each document
+  # that appears on the catalog index page.
+  # If the object is a template, override the normal blacklight
+  # config and display the template_title_tesim instead.
   def document_show_link_field(document=nil)
     field = if document[:template_title_tesim]
               :template_title_tesim
@@ -9,6 +13,10 @@ module BlacklightHelper
             end
   end
 
+  # This method controls the title that appears at the top
+  # of the show page for an object.
+  # If the object is a template, override the normal blacklight
+  # config and display the template_title_tesim instead.
   def document_heading(document=nil)
     document ||= @document
     label = document[:template_title_tesim]
