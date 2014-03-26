3613325449600bd79daf122b8edbad36b1ed0776
Mark Bussey <mark@curationexperts.com>
2014-03-26 16:34:55 -0500

    Add template controller and logic to filter only templates
    Also adds links on home screen - closes #149

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,10 +1,14 @@
-class TemplatesController < ApplicationController
+class TemplatesController < CatalogController
 
   # Since TuftsTemplates behave just like other fedora
   # objects, most of the template actions are handled by
   # either the CatalogController or the RecordsController.
 
-  def index
+  TemplatesController.solr_search_params_logic += [:only_templates]
+
+  def only_templates(solr_parameters, user_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << "active_fedora_model_ssi:TuftsTemplate"
   end
 
 end
