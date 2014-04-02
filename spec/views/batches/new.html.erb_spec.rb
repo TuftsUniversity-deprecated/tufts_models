96ca84c48008ee532b05ccd3ed7428e201beefd1
val99erie <val99erie@gmail.com>
2014-04-02 10:03:46 -0500

    Refactored views for batch forms

--- spec/views/batches/new.html.erb_spec.rb
+++ spec/views/batches/new.html.erb_spec.rb
@@ -1,30 +1,21 @@
 require 'spec_helper'
 
 describe "batches/new.html.erb" do
-  describe 'apply_template' do
+
+  context 'Batch type: BatchTemplateUpdate' do
     before do
-      @templates = [
+      templates = [
         FactoryGirl.create(:tufts_template),
         FactoryGirl.create(:tufts_template)
       ]
-      @pids = ["tufts:1", "tufts:2"]
-      assign :batch, Batch.new(pids: @pids, type: 'BatchTemplateUpdate')
+      pids = ["tufts:1", "tufts:2"]
+      assign :batch, Batch.new(pids: pids, type: 'BatchTemplateUpdate')
       render
     end
 
-    it 'submits to batch#create' do
-      rendered.should have_selector("form[method=post][action='#{batches_path}']")
-    end
-
-    it 'displays the form to apply a template' do
-      expect(rendered).to have_selector("input[type=hidden][name='batch[type]'][value=BatchTemplateUpdate]")
-      expect(rendered).to have_selector("select[name='batch[template_id]']")
-      @pids.each do |pid|
-        expect(rendered).to have_selector("input[type=hidden][name='batch[pids][]'][value='#{pid}']")
-      end
-      @templates.each do |t|
-        rendered.should have_selector("option[value='#{t.id}']")
-      end
+    it 'renders the form for BatchTemplateUpdate' do
+      expect(view.rendered_views.rendered_views).to include('batches/form_batch_template_update')
     end
   end
+
 end
