31c0d403f410e7cd1990ab5060834aec124f0bd7
val99erie <val99erie@gmail.com>
2014-04-14 15:59:26 -0500

    Specs for catalog show related to jquery file upload

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -49,34 +49,18 @@ describe CatalogController do
       response.should render_template(:show)
     end
 
-    it 'fix me' do
-      pending
+    describe 'GET show JSON' do
+      it 'returns JSON data needed by the view template (jquery file uploader template)' do
+        record = FactoryGirl.create(:tufts_pdf, user: @admin)
+        get :show, id: record.pid, format: :json, json_format: 'jquery-file-uploader'
+
+        expect(response).to be_successful
+        json = JSON.parse(response.body)['files'].first
+        expect(json['pid']).to eq record.pid
+        expect(json['name']).to eq record.title
+        record.delete
+      end
     end
-#    describe 'GET show JSON' do
-#      it 'returns a json document' do
-#        record = FactoryGirl.create(:tufts_pdf, user: @admin)
-#        get :show, id: record.pid, format: :json
-#
-#        expect(response).to be_successful
-#
-#        puts '--------------'
-#        pp JSON.parse(response.body)
-#
-#        json = JSON.parse(response.body)['files'].first
-#        expect(json['id']).to eq record.pid
-#        expect(json['name']).to eq record.title
-#        expect(json['url']).to eq catalog_path(record.pid)
-#
-#        record.delete
-#      end
-#
-#      it 'request a non-existent record returns not found' do
-#        pid = 'fake_pid:123'
-#        ActiveFedora::Base.delete(pid) if ActiveFedora::Base.exists?(pid)
-#        get :show, id: pid, format: :json
-#        expect(response).to be_not_found
-#      end
-#    end
 
     context 'viewing templates' do
       before { @excluded = FactoryGirl.create(:tufts_template) }
