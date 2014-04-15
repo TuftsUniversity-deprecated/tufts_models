76ee4de987bf5d7700de2a324fd988969dfc9f09
val99erie <val99erie@gmail.com>
2014-04-15 11:45:55 -0500

    Batch status for template import

--- app/helpers/batches_helper.rb
+++ app/helpers/batches_helper.rb
@@ -21,4 +21,14 @@ module BatchesHelper
       job.status.capitalize
     end
   end
+
+  def line_item_status(batch, job, record_id=nil)
+    if batch.is_a?(BatchTemplateImport) || batch.is_a?(BatchXmlImport)
+      record_exists = ActiveFedora::Base.exists?(record_id)
+      record_exists ? 'Completed' : 'Status not available'
+    else
+      job_status_text(@batch, job)
+    end
+  end
+
 end
