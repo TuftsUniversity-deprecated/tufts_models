2e3c6c7f6d465984c7bd30d560b299edb131e593
Brian Maddy <brian@brianmaddy.com>
2014-04-24 18:43:45 -0500

    using format_attr instead of format in adv search due to a conflict with rails controllers #151

--- app/controllers/concerns/advanced_search_fields.rb
+++ app/controllers/concerns/advanced_search_fields.rb
@@ -10,16 +10,17 @@ module AdvancedSearchFields
       already_included_attrs = [:title, :creator, :subject, :batch]
       adv_search_attrs = adv_search_attrs - already_included_attrs
 
-      # TODO:  I removed the format field from the search form
-      # because it causes the controller to raise an exception.
-      # We'll need to handle the format attribute separately.
-      adv_search_attrs = adv_search_attrs - [:format]
-
       adv_search_attrs.each do |attr|
         field_name = attr.to_s.underscore
         config.add_search_field(field_name) do |field|
           field.include_in_simple_select = false
           field.solr_local_parameters = { :qf => field_name + '_tesim' }
+          # using :format_attr for :format because :format refers to the response
+          # format in rails controllers
+          if attr == :format
+            field.field = "format_attr"
+            field.label = "Format"
+          end
         end
       end
     end
