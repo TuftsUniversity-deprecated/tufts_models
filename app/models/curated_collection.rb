aaacc70a5a1944751cbfc1d552de07c609322968
Justin Coyne <justin@curationexperts.com>
2014-06-26 09:27:56 -0500

    CuratedCollection does not need to descent from baseModel

--- app/models/curated_collection.rb
+++ app/models/curated_collection.rb
@@ -1,10 +1,16 @@
 class CuratedCollection < ActiveFedora::Base
-  include BaseModel
   include WithValidDisplays
 
   validates :title, presence: true
   after_initialize :default_attributes
+
+  has_metadata "DCA-META", type: TuftsDcaMeta
   has_metadata 'collectionMetadata', type: CollectionMetadata
+  has_metadata "DCA-ADMIN", type: DcaAdmin
+
+  has_attributes :creator, :description, :date_created, datastream: 'DCA-META', multiple: true
+  has_attributes :title, datastream: 'DCA-META', multiple: false
+  has_attributes :displays, datastream: 'DCA-ADMIN', multiple: true
 
   delegate :members, :member_ids, to: :collectionMetadata
 
