360b3725c12f723315354662b492b1200b702b85
Justin Coyne <justin@curationexperts.com>
2014-06-26 10:03:39 -0500

    enforce access controls in TIL

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -8,7 +8,9 @@ class CatalogController < ApplicationController
   before_filter :enforce_show_permissions, :only => :show
 
   # This applies appropriate access controls to all solr queries
-  #CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  if Tufts::Application.til?
+    CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  end
 
   # This filters out objects that you want to exclude from search results, like FileAssets
   CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
