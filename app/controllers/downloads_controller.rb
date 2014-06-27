012b36f2af244b829ffb430cf6169ca8a0f6166f
Justin Coyne <justin@curationexperts.com>
2014-06-26 20:57:01 -0500

    In TIL on an image show page, the image takes up 6 cols

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,8 +1,15 @@
 class DownloadsController < ApplicationController
   include Hydra::Controller::DownloadBehavior
+
+  def render_404
+    respond_to do |format|
+      format.any  { send_file 'app/assets/images/nope.png', disposition: 'inline', type: 'image/png' }
+    end
+  end
+  
   def send_content
     send_file asset.local_path_for(params[:datastream_id]), content_options
-  end    
+  end
 
   # Create some headers for the datastream
   def content_options
--- app/helpers/blacklight_configuration_helper.rb
+++ app/helpers/blacklight_configuration_helper.rb
@@ -2,7 +2,7 @@ module BlacklightConfigurationHelper
   include Blacklight::ConfigurationHelperBehavior
 
   def document_show_fields document=nil
-    if document.image?
+    if Tufts::Application.til? && document.image?
       # archival and/or curated collection name (if possible) (optional)
       # for admin users: show what personal collections this image is a part of (optional)
       blacklight_config.show_fields.select { |k, _| ['creator_tesim', 'date_created_tesim', 'description_tesim'].include? k}
