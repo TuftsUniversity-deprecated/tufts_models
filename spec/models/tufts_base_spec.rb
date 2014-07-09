daa169db4fc91d71027ff6957f4f1b045679471f
Mark Bussey <mark@curationexperts.com>
2014-07-09 15:36:45 -0500

    Refactor tufts_base_spec and add 'tdil' display test

--- spec/models/tufts_base_spec.rb
+++ spec/models/tufts_base_spec.rb
@@ -20,6 +20,83 @@ describe TuftsBase do
     end
   end
 
+  describe "to_solr" do
+    describe "when not saved" do
+      before do
+        subject.stub(:pid => 'changeme:999')
+      end
+
+      describe "subject field" do
+        it "should save both" do
+          subject.subject = "subject1"
+          subject.funder = "subject2"
+          solr_doc = subject.to_solr
+          solr_doc["subject_tesim"].should == ["subject1"]
+          solr_doc["funder_tesim"].should == ["subject2"]
+          # TODO is this right? Presumably this is for the facet
+          solr_doc["subject_sim"].should == ["Subject1"]
+        end
+      end
+
+      describe "displays" do
+        it "should save it" do
+          subject.displays = "dl"
+          solr_doc = subject.to_solr
+          solr_doc['displays_ssi'].should == 'dl'
+        end
+      end
+
+      describe "title" do
+        it "should be searchable and facetable" do
+          subject.title = "My title"
+          solr_doc = subject.to_solr
+          solr_doc['title_si'].should == 'My title'
+          solr_doc['title_tesim'].should == ['My title']
+        end
+      end
+
+      describe "contributor added" do
+        it "should save it" do
+          subject.contributor = "Michael Jackson"
+          solr_doc = subject.to_solr
+          solr_doc['names_sim'].should == ['Michael Jackson']
+        end
+      end
+    end
+
+    describe "date added" do
+      before do
+        subject.save(validate: false)
+        @solr_doc = subject.to_solr
+      end
+      it "should be sortable" do
+        @solr_doc['system_create_dtsi'].should_not be_nil
+      end
+    end
+  end
+
+  describe "displays" do
+    it "should only allow one of the approved values" do
+      subject.title = 'test title' #make it valid
+      subject.displays = 'dl'
+      subject.should be_valid
+      subject.displays = 'tisch'
+      subject.should be_valid
+      subject.displays = 'aah'
+      subject.should be_valid
+      subject.displays = 'perseus'
+      subject.should be_valid
+      subject.displays = 'elections'
+      subject.should be_valid
+      subject.displays = 'dark'
+      subject.should be_valid
+      subject.displays = 'tdil'
+      subject.should be_valid
+      subject.displays = 'fake'
+      subject.should_not be_valid
+    end
+  end
+
   describe 'namespace' do
     it 'correctly prefixes DC terms' do
       dc_attributes = [:title, :creator, :source, :description,
