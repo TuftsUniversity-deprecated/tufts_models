761a3aecede659799715ce8250e4fc744eda2ddd
Brian Maddy <brian@brianmaddy.com>
2014-08-26 10:48:26 -0500

    added explicit local namespace

--- app/models/datastreams/dca_admin.rb
+++ app/models/datastreams/dca_admin.rb
@@ -1,33 +1,33 @@
 class DcaAdmin < ActiveFedora::OmDatastream
   set_terminology do |t|
-    t.root(:path => "admin", 'xmlns'=>"http://nils.lib.tufts.edu/dcaadmin/", 'xmlns:ac'=>"http://purl.org/dc/dcmitype/")
+    t.root(:path => "admin", 'xmlns:local'=>"http://nils.lib.tufts.edu/dcaadmin/", 'xmlns:ac'=>"http://purl.org/dc/dcmitype/")
 
     t.template_name index_as: :stored_searchable, path: 'templateName'
-    t.steward index_as: :stored_searchable
+    t.steward namespace_prefix: "local", index_as: :stored_searchable
     t.name namespace_prefix: "ac", index_as: :stored_searchable
     t.comment namespace_prefix: "ac", index_as: :stored_searchable
-    t.retentionPeriod index_as: :stored_searchable
-    t.displays index_as: [:stored_sortable, :symbol]
-    t.embargo index_as: :dateable
-    t.status index_as: :stored_searchable
-    t.startDate index_as: :stored_searchable
-    t.expDate index_as: :stored_searchable
-    t.qrStatus index_as: [:stored_searchable, :facetable]
-    t.rejectionReason index_as: :stored_searchable
-    t.note index_as: :stored_searchable
-    t.createdby
+    t.retentionPeriod namespace_prefix: "local", index_as: :stored_searchable
+    t.displays namespace_prefix: "local", index_as: [:stored_sortable, :symbol]
+    t.embargo namespace_prefix: "local", index_as: :dateable
+    t.status namespace_prefix: "local", index_as: :stored_searchable
+    t.startDate namespace_prefix: "local", index_as: :stored_searchable
+    t.expDate namespace_prefix: "local", index_as: :stored_searchable
+    t.qrStatus namespace_prefix: "local", index_as: [:stored_searchable, :facetable]
+    t.rejectionReason namespace_prefix: "local", index_as: :stored_searchable
+    t.note namespace_prefix: "local", index_as: :stored_searchable
+    t.createdby namespace_prefix: "local"
 
-    t.published_at path: "publishedAt", type: :time, index_as: :stored_sortable
-    t.edited_at path: "editedAt", type: :time, index_as: :stored_sortable
-    t.creatordept
-    t.batch_id index_as: :symbol, :path => 'batchID'
+    t.published_at namespace_prefix: "local", path: "publishedAt", type: :time, index_as: :stored_sortable
+    t.edited_at namespace_prefix: "local", path: "editedAt", type: :time, index_as: :stored_sortable
+    t.creatordept namespace_prefix: "local"
+    t.batch_id namespace_prefix: "local", index_as: :symbol, :path => 'batchID'
   end
 
   # BUG?  Extra solr fields are generated when there is a default namespace (xmlns) declared on the root.
   #   compared to when the root has a namespace and the child elements do not have an namespace.
 
   def self.xml_template
-    Nokogiri::XML('<admin xmlns="http://nils.lib.tufts.edu/dcaadmin/" xmlns:ac="http://purl.org/dc/dcmitype/"/>')
+    Nokogiri::XML('<admin xmlns:local="http://nils.lib.tufts.edu/dcaadmin/" xmlns:ac="http://purl.org/dc/dcmitype/"/>')
   end
 
   def prefix
