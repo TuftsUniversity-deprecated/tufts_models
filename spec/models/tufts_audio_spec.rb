e496b4d2fa4c0f9a50f0551fbdfbe9ea30a02569
Justin Coyne <justin@curationexperts.com>
2014-09-24 11:12:18 -0500

    Copied tests from MIRA commit: https://github.com/curationexperts/mira/commit/3a891ad8808774581a6cf2f689cf3325a374dca4

--- spec/models/tufts_audio_spec.rb
+++ spec/models/tufts_audio_spec.rb
@@ -2,6 +2,81 @@ require 'spec_helper'
 
 describe TuftsAudio do
 
+  describe "to_solr" do
+    describe "when not saved" do
+      before do
+        allow(subject).to receive(:pid).and_return('changeme:999')
+      end
+      describe "subject field" do
+        it "should save both" do
+          subject.subject = ["subject1"]
+          subject.funder = ["subject2"]
+          solr_doc = subject.to_solr
+          expect(solr_doc["subject_tesim"]).to eq ["subject1"]
+          expect(solr_doc["funder_tesim"]).to eq ["subject2"]
+          # TODO is this right? Presumably this is for the facet
+          expect(solr_doc["subject_sim"]).to eq ["Subject1"]
+        end
+      end
+
+      describe "displays" do
+        it "should save it" do
+          subject.displays = ["dl"]
+          solr_doc = subject.to_solr
+          expect(solr_doc['displays_ssim']).to eq ['dl']
+        end
+      end
+      describe "title" do
+        it "should be searchable and facetable" do
+          subject.title = "My title"
+          solr_doc = subject.to_solr
+          expect(solr_doc['title_si']).to eq 'My title'
+          expect(solr_doc['title_tesim']).to eq ['My title']
+        end
+      end
+
+      describe "contributor added" do
+        it "should save it" do
+          subject.contributor = ["Michael Jackson"]
+          solr_doc = subject.to_solr
+          expect(solr_doc['names_sim']).to eq ['Michael Jackson']
+        end
+      end
+    end
+
+    describe "date added" do
+      before do
+        subject.save(validate: false)
+      end
+      let(:solr_doc) { subject.to_solr }
+
+      it "should be sortable" do
+        expect(solr_doc['system_create_dtsi']).to_not be_nil
+      end
+    end
+  end
+
+  describe "displays" do
+    it "should only allow one of the approved values" do
+      subject.title = 'test title' #make it valid
+      subject.displays = ['dl']
+      expect(subject).to be_valid
+      subject.displays = ['tisch']
+      expect(subject).to be_valid
+      subject.displays = ['aah']
+      expect(subject).to be_valid
+      subject.displays = ['perseus']
+      expect(subject).to be_valid
+      subject.displays = ['elections']
+      expect(subject).to be_valid
+      subject.displays = ['dark']
+      expect(subject).to be_valid
+      subject.displays = ['fake']
+      expect(subject).to_not be_valid
+    end
+  end
+
+
   describe "terms_for_editing" do
     it "has the correct values" do
       expect(subject.terms_for_editing).to eq [:identifier, :title, :alternative, :creator, :contributor, :description, :abstract, :toc, :publisher, :source, :date, :date_created, :date_copyrighted, :date_submitted, :date_accepted, :date_issued, :date_available, :date_modified, :language, :type, :format, :extent, :medium, :persname, :corpname, :geogname, :subject, :genre, :provenance, :rights, :access_rights, :rights_holder, :license, :replaces, :isReplacedBy, :hasFormat, :isFormatOf, :hasPart, :isPartOf, :accrualPolicy, :audience, :references, :spatial, :bibliographic_citation, :temporal, :funder, :resolution, :bitdepth, :colorspace, :filesize, :steward, :name, :comment, :retentionPeriod, :displays, :embargo, :status, :startDate, :expDate, :qrStatus, :rejectionReason, :note, :createdby, :creatordept]
