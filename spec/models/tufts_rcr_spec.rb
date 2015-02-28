988799bfe102d54fa23c793d147e997114e7ae9e
Mike Korcynski <mkorcy@gmail.com>
2015-02-28 11:44:49 -0500

    fills out the spec for RCR model

--- spec/models/tufts_rcr_spec.rb
+++ spec/models/tufts_rcr_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe TuftsRCR do
+  it "should have an original_file_datastreams" do
+    expect(TuftsRCR.original_file_datastreams).to eq ['RCR-CONTENT']
+  end
+ 
+  describe "to_class_uri" do
+    subject {TuftsRCR}
+    it "has sets the class_uri" do
+      expect(subject.to_class_uri).to eq 'info:fedora/cm:Text.RCR'
+    end
+  end
 end
