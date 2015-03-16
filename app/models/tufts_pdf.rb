415bc32dc4a2814a67098e59ca92c76fb236cf2d
Mike Korcynski <mkorcy@gmail.com>
2015-03-16 14:05:43 -0400

    removes pages methods out to a concern
    also reorganizes logging code and breaks down the page creation
    methods a bit.

--- app/models/tufts_pdf.rb
+++ app/models/tufts_pdf.rb
@@ -1,6 +1,9 @@
 class TuftsPdf < TuftsBase
-  has_file_datastream 'Archival.pdf', control_group: 'E', original: true
 
+  PDF_CONTENT_DS = 'Archival.pdf'
+
+  has_file_datastream PDF_CONTENT_DS, control_group: 'E', original: true
+  include WithPageImages
 
   # @param [String] _ Datastream id - not used
   # @param [String] type the content type to test
@@ -11,7 +14,7 @@ class TuftsPdf < TuftsBase
 
 
   def self.valid_pdf_mime_type?(type)
-    %Q{application/pdf application/x-pdf application/acrobat applications/vnd.pdf text/pdf text/x-pdf}.include?(type)
+    %q{application/pdf application/x-pdf application/acrobat applications/vnd.pdf text/pdf text/x-pdf}.include?(type)
   end
 
 
@@ -19,107 +22,113 @@ class TuftsPdf < TuftsBase
     'info:fedora/cm:Text.PDF'
   end
 
-
-  def local_path_for_pdf_derivatives
-    # Convert the local path for the archival pdf into the local path for the derivatives folder:
-    #   - replace '/data01/' or '/data05/' with '/dcadata02/';
-    #   - if the path contains '/facpubs/' replace '/archival_pdf/' with '/access_pdf_pageimages/facpubs.', otherwise with '/access_pdf_pageimages/';
-    #   - replace '.archival.pdf' with nothing.
-
-    derivatives_path = local_path_for('Archival.pdf')
-    
-    derivatives_path.sub(/\/data\d\d\//, '/dcadata02/').sub(/\/archival_pdf\//, '/access_pdf_pageimages/' + (derivatives_path.match(/\/facpubs\//) ? 'facpubs.' : '')).sub(/\.archival\.pdf/, '')
-  end
-
-
-  def local_path_for_readme(derivatives_path = local_path_for_pdf_derivatives)
-    derivatives_path + '/readme.txt'
-  end
-
-
-  def local_path_for_book_meta(derivatives_path = local_path_for_pdf_derivatives)
-    derivatives_path + '/book_meta.json'
-  end
-
-
-  def local_path_for_png(page_number, derivatives_path = local_path_for_pdf_derivatives, filename_base = pid_without_namespace)
-    derivatives_path + '/' + filename_base + "-" + page_number.to_s + '.png'
-  end
-
-
   # To cause create_derivatives to be invoked, execute this line of code in rails console:
   # Job::CreateDerivatives.new('uuid', 'record_id' => '<pid beginning with tufts:>').perform
   def create_derivatives
+
+
     begin
-      log_file = File.open(Rails.root.join('log', 'derivatives.log'), 'a')
-      log_file.sync = true  # flush each log message immediately
-      logger = Logger.new(log_file)
-      logger.formatter = proc do |severity, datetime, progname, msg| "#{msg}\n" end
-      logger.info(DateTime.parse(Time.now.to_s).strftime('%A %B %-d, %Y %I:%M:%S %p'))
 
-      file_asset = ActiveFedora::Base.find(pid)
+      if datastreams.include? PDF_CONTENT_DS
+        derivative_settings = setup_derivative_environment
 
-      if file_asset.nil?
-        logger.fatal('Can\'t create PDF derivatives for ' + pid + '.  Object not found.')
+        create_page_turner_json derivative_settings
+        create_readme derivative_settings
+        create_pdf_page_images derivative_settings
+
       else
-        unless file_asset.datastreams.include?('Archival.pdf')
-          logger.fatal('Can\'t create PDF derivatives for ' + pid + '.  Object does not have an Archival.pdf datastream.')
-        else
-          pdf_path = local_path_for('Archival.pdf')
-          logger.info('Creating PDF derivatives for ' + pid + '.')
-
-          pdf_pages = Magick::Image.read(pdf_path){self.density = '150x150'}  # meaning 150 dpi
-          logger.info('  Archival PDF is at ' + pdf_path + '.')
-
-          page_count = pdf_pages.length
-          page_width = pdf_pages[0].columns
-          page_height = pdf_pages[0].rows
-          logger.info('  Found ' + page_count.to_s + ' page' + (page_count == 1 ? '' : 's') + ' (' + page_width.to_s + ' x ' + page_height.to_s + ').')
-
-          derivatives_path = local_path_for_pdf_derivatives()
-          FileUtils.mkdir_p(derivatives_path)
-
-          book_meta_path = local_path_for_book_meta(derivatives_path)
-          book_meta_json = '{"page_width":"' + page_width.to_s + '","page_height":"' + page_height.to_s + '","page_count":"' + page_count.to_s + '"}'
-          logger.info('  Writing ' + book_meta_json + ' to ' + book_meta_path + '.')
-          File.open(book_meta_path, 'w'){|file| file.puts(book_meta_json)}
-
-          readme_path = local_path_for_readme(derivatives_path)
-          readme_text = 'Created by MIRA from source: ' + pdf_path
-          logger.info('  Writing ' + readme_text + ' to ' + readme_path + '.')
-          File.open(readme_path, 'w'){|file| file.puts(readme_text)}
-
-          page_number = 0
-          filename_base = pid_without_namespace
-
-          pdf_pages.each do |pdf_page|
-            png_path = local_path_for_png(page_number, derivatives_path, filename_base)
-            logger.info('  Writing ' + png_path + '.')
-
-            pdf_page.write(png_path){self.quality = 100}  # meaning 100% quality
-            pdf_page.destroy!  # this is important - without it RMagick can occasionally be left in a state that causes subsequent failures
-            pdf_pages[page_number] = nil
-
-            page_number += 1
-          end
-
-        logger.info('Successfully created PDF derivatives for ' + pid + '.')
-        end
+        logger.error("Can't create PDF derivatives for #{pid}.  Object does not have an Archival.pdf datastream.")
       end
+
     rescue Magick::ImageMagickError => ex
-      logger.fatal('Can\'t create PDF derivatives for ' + pid +  '.  ImageMagick error: ' + ex.message)
+      logger.error("Can't create PDF derivatives for #{pid}.  ImageMagick error: #{ex.message}")
       raise(ex)
     rescue SystemCallError => ex
-      logger.fatal('Can\'t create PDF derivatives for ' + pid +  '.  I/O error: ' + ex.message)
+      logger.error("Can't create PDF derivatives for #{pid}.  I/O error: #{ex.message}")
       raise(ex)
     rescue StandardError => ex
-      logger.fatal('Can\'t create PDF derivatives for ' + pid +  '.  error: ' + ex.message)
+      logger.error("Can't create PDF derivatives for #{pid}.  error: #{ex.message}")
       raise(ex)
     ensure
       logger.info(DateTime.parse(Time.now.to_s).strftime('%A %B %-d, %Y %I:%M:%S %p'))
-      logger.info('')  # blank line between events
+      logger.info('') # blank line between events
     end
   end
 
+  private
+
+  def setup_derivative_environment
+    derivative_settings = Hash.new
+
+    pdf_path = derivative_settings[:pdf_path] = local_path_for PDF_CONTENT_DS
+
+    DERIVATIVES_LOGGER.info("Creating PDF derivatives for #{pid}.")
+
+    derivative_settings[:pdf] = Magick::Image.read(pdf_path) { self.density = '150x150' } # meaning 150 dpi
+
+    DERIVATIVES_LOGGER.info("  Archival PDF is at #{pdf_path}.")
+
+    derivative_settings[:pdf_meta] = extract_pdf_metadata derivative_settings
+    derivative_settings[:derivatives_path] = create_derivatives_directory
+
+    derivative_settings
+  end
+
+  def extract_pdf_metadata(derivative_settings)
+    pdf_meta = Hash.new
+
+    pdf = derivative_settings[:pdf]
+    pdf_meta[:page_count] = pdf.length.to_s
+    pdf_meta[:page_width] = pdf[0].columns.to_s
+    pdf_meta[:page_height] = pdf[0].rows.to_s
+
+    DERIVATIVES_LOGGER.info("  Found #{pdf_meta[:page_count]} page(s) (#{pdf_meta[:page_width]} x #{pdf_meta[:page_height]}).")
+
+    pdf_meta
+  end
+
+  def create_derivatives_directory
+    derivatives_path = local_path_for_pdf_derivatives
+    FileUtils.mkdir_p(derivatives_path) #returns list
+    derivatives_path
+  end
+
+  def create_pdf_page_images(derivative_settings)
+    page_number = 0
+    filename_base = pid_without_namespace
+    pdf_pages = derivative_settings[:pdf]
+
+    pdf_pages.each do |pdf_page|
+      png_path = local_path_for_png(page_number, derivative_settings[:derivatives_path], filename_base)
+
+      DERIVATIVES_LOGGER.info("  Writing #{png_path}.")
+
+      pdf_page.write(png_path) { self.quality = 100 } # meaning 100% quality
+      pdf_page.destroy! # this is important - without it RMagick can occasionally be left in a state that causes subsequent failures
+      pdf_pages[page_number] = nil
+
+      page_number += 1
+    end
+
+    DERIVATIVES_LOGGER.info("Successfully created PDF derivatives for #{pid}.")
+  end
+
+  def create_page_turner_json(derivative_settings)
+    book_meta_path = local_path_for_book_meta(derivative_settings[:derivatives_path])
+    book_meta_json = derivative_settings[:pdf_meta].to_json
+
+    DERIVATIVES_LOGGER.info("Writing #{book_meta_json}  to #{book_meta_path}.")
+
+    File.open(book_meta_path, 'w') { |file| file.puts(book_meta_json) }
+  end
+
+  def create_readme(derivatives_settings)
+    readme_path = local_path_for_readme(derivatives_settings[:derivatives_path])
+    readme_text = "Created by MIRA from source: #{derivatives_settings[:pdf_path]}"
+
+    DERIVATIVES_LOGGER.info("  Writing #{readme_text} to  #{readme_path}.")
+
+    File.open(readme_path, 'w') { |file| file.puts(readme_text) }
+  end
 
 end
