e5f5bacf1fb212e6ea4d87d1fe0025360c7e4c5c
Mike Korcynski <mkorcy@gmail.com>
2015-03-31 11:13:32 -0400

    removes facpub and associated spec

--- app/models/tufts_faculty_publication.rb
+++
@@ -1,6 +0,0 @@
-class TuftsFacultyPublication < TuftsBase 
-  has_file_datastream 'Archival.pdf', control_group: 'E', original: true
-  def self.to_class_uri
-    'info:fedora/cm:Text.FacPub'
-  end
-end
