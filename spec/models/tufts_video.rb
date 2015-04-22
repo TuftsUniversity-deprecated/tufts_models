43b5ff8dfaed47ed31b62f672a21f232f5c21260
Mike Korcynski <mkorcy@gmail.com>
2015-04-22 11:06:28 -0400

    adds additional specs for video model

--- spec/models/tufts_video.rb
+++ spec/models/tufts_video.rb
@@ -7,12 +7,12 @@ describe TuftsVideo do
   end
 
   it "should have an original_file_datastreams" do
-    expect(TuftsVideo.original_file_datastreams).to eq ['Archival.xml']
+    expect(TuftsVideo.original_file_datastreams).to eq ['ARCHIVAL_XML','Archival.video']
   end
 
 
   describe '#to_solr' do
-    subject { TuftsTEI.create(pid: 'tufts:ms102.001', title: 'some title') }
+    subject { TuftsVideo.create(pid: 'tufts:ua236.001', title: 'some video') }
 
     before do
       subject.add_relationship(:has_model, 'info:fedora/afmodel:TuftsVideo')
@@ -21,6 +21,18 @@ describe TuftsVideo do
     it 'sets object type as Video' do
       solr_doc = subject.to_solr
       expect(solr_doc['object_type_sim']).to eq ['Video']
+    end
+  end
+
+  describe "#valid_type_for_datastream?" do
+    subject { TuftsVideo.create(pid: 'tufts:ua236.001', title: 'some video') }
+
+    it 'should not allow you to upload an invalid type for a video' do
+     expect(subject.valid_type_for_datastream?('Archival.video','image/png')).to eq false
+    end
+
+    it 'should allow you to upload an valid type for a video' do
+      expect(subject.valid_type_for_datastream?('Archival.video','video/mp4')).to eq true
     end
   end
 end
