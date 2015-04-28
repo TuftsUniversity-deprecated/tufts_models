a1312266aa50bd45edce6e5befd7f5038ec60051
Justin Coyne <justin@curationexperts.com>
2015-04-28 16:22:12 -0500

    Extract Batch business logic to a service

--- app/models/batch_publish.rb
+++ app/models/batch_publish.rb
@@ -6,11 +6,7 @@ class BatchPublish < Batch
   end
 
   def run
-    return false unless valid?
-    ids = pids.map do |pid|
-      job = Job::Publish.create(user_id: creator.id, record_id: pid, batch_id: id)
-    end
-    update_attribute(:job_ids, ids)
+    BatchRunnerService.new(self).run
   end
 
 end
