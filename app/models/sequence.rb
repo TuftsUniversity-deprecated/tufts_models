f8f9c60edc6283e9abe3c967b0536c27ae1eec49
Justin Coyne <justin@curationexperts.com>
2015-04-29 14:53:09 -0500

    Add scope to sequence

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -1,8 +1,8 @@
 class Sequence < ActiveRecord::Base
   def self.next_val options = {}
     namespace = options.fetch(:namespace, 'tufts')
-    format = "#{namespace}:sd.%07d"
-    seq = Sequence.first_or_create
+    format = options.fetch(:format, "#{namespace}:sd.%07d")
+    seq = Sequence.find_or_create_by(scope: options[:scope])
     seq.with_lock do
       seq.value += 1
       seq.save!
--- spec/models/sequence_spec.rb
+++ spec/models/sequence_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe Sequence do
+  before { Sequence.delete_all }
+
   it "should have a format like tufts:sd.0000000" do
     expect(Sequence.next_val).to match /^tufts:sd\.\d{7}$/
+  end
+
+  describe "scope" do
+    it "makes the sequences independent" do
+      one = Sequence.next_val(scope: 'handle', format: '%d')
+      two = Sequence.next_val(format: '%d')
+      expect(one).to eq two
+      three = Sequence.next_val(format: '%d')
+      expect(two).not_to eq three
+    end
+  end
+
+  context "with scope and format" do
+    subject { Sequence.next_val(scope: 'handle', format: '%d') }
+    it { is_expected.to eq '1' }
   end
 end
