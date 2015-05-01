7d4ece0ed0c45b8cc5caa6ab98b39d12021139e8
Justin Coyne <justin@curationexperts.com>
2015-05-01 11:01:32 -0500

    Refactor logging so the handle log can extend from it.

--- app/services/audit_log_service.rb
+++ app/services/audit_log_service.rb
@@ -1,13 +1,5 @@
-module AuditLogService
-  def self.log(who, what, message)
-    logger.info("#{what} | #{message} | #{who}")
-  end
-
-  def self.logger
-    @@logger ||= Logger.new(filename, 'monthly')
-  end
-
-  def self.filename
+class AuditLogService < LogService
+  def filename
     Rails.root + 'log' + 'audit.log'
   end
 end
