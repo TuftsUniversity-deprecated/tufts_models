5370909b902eee52131ce49c484c30e4a2c3a6b0
Mike Korcynski <mkorcy@gmail.com>
2015-11-10 10:59:49 -0500

    makes rcrs layout correctly on disk

--- app/models/tufts_rcr.rb
+++ app/models/tufts_rcr.rb
@@ -5,4 +5,11 @@ class TuftsRCR < TuftsBase
     'info:fedora/cm:Text.RCR'
   end
 
+  def file_path(name, extension=nil)
+    File.join(directory_for(name), "#{PidUtils.stripped_pid(pid)}.xml")
+  end
+
+  def directory_for(name)
+    File.join('RCR', 'rcr-content')
+  end
 end
